b28d275c1427340f8e0afedff851fc1b6e062ba0
Yotam Korah <yotamkorah@gmail.com>
2023-07-21 17:27:46 +0300

    wip - resume page finished, started contact page

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,12 +1,18 @@
-<script>
+<script lang="ts">
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, DarkMode } from 'flowbite-svelte';
 	import logo from '$lib/images/closeUp.jpg';
 	import { page } from '$app/stores';
+
+	const toggleOnMobile = (toggleCallback: (e: MouseEvent) => void) => {
+		if (window.innerWidth <= 640) {
+			return toggleCallback;
+		}
+	};
 </script>
 
 <header class="fixed top-0 left-0 right-0">
 	<Navbar
-		navClass="px-2 sm:px-4 py-2.5 absolute w-full z-20 top-0 left-0 border-b"
+		navClass="px-2 sm:px-4 py-2.5 absolute w-full z-20 top-0 left-0 border-b dark:bg-gray-900"
 		let:hidden
 		let:toggle
 	>
@@ -19,10 +25,10 @@
 		</NavBrand>
 		<NavHamburger on:click={toggle} />
 		<NavUl {hidden}>
-			<NavLi href="/" active={$page.url.pathname === '/'}>Home</NavLi>
-			<NavLi href="/projects" active={$page.url.pathname === '/projects'}>Projects</NavLi>
-			<NavLi href="/cv" active={$page.url.pathname === '/cv'}>CV</NavLi>
-			<NavLi href="/contact" active={$page.url.pathname === '/contact'}>Contact</NavLi>
+			<NavLi href="/" active={$page.url.pathname === '/'} on:click={toggleOnMobile(toggle)}>Home</NavLi>
+			<NavLi href="/projects" active={$page.url.pathname === '/projects'} on:click={toggleOnMobile(toggle)}>Projects</NavLi>
+			<NavLi href="/resume" active={$page.url.pathname === '/resume'} on:click={toggleOnMobile(toggle)}>Resume</NavLi>
+			<NavLi href="/contact" active={$page.url.pathname === '/contact'} on:click={toggleOnMobile(toggle)}>Contact Me</NavLi>
 			<DarkMode
 				class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg text-lg"
 				style="padding: 0.5rem"
---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import resume from '$lib/documents/resume.pdf';
+</script>
+
+<svelte:head>
+	<title>Yotam's Resume</title>
+	<meta name="description" content="Updated Resume" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Everything In One Place</h1>
+
+	<iframe title="Yotam's Resume" src={resume} class="h-96" allowfullscreen >
+		<p style="font-size: 110%;">
+			<em
+				><strong>ERROR: </strong>
+				An &#105;frame should be displayed here but your browser version does not support &#105;frames.
+			</em>Please update your browser to its most recent version and try again.
+		</p>
+	</iframe>
+</div>
